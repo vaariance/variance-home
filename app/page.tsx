86c00fccf5b99f1be03eabe61d300e9ee6cb74b7
Miracle Onyenma <42788603+miracleonyenma@users.noreply.github.com>
2023-11-23 08:55:11 +0000

    updates: set up

---
+++ app/page.tsx
@@ -0,0 +1,437 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="index">
+        <div className="div">
+          <div className="overlap-group">
+            <div className="text-wrapper">PARTNERS</div>
+            <Image
+              width={100}
+              height={100}
+              className="clip-path-group"
+              alt="Clip path group"
+              src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/clip-path-group-8@2x.png"
+            />
+            <div className="text-wrapper-2">PARTNER 1</div>
+            <div className="text-wrapper-3">PARTNER 2</div>
+            <Image
+              width={100}
+              height={100}
+              className="img"
+              alt="Clip path group"
+              src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/clip-path-group-8@2x.png"
+            />
+            <div className="text-wrapper-4">PARTNER 3</div>
+            <Image
+              width={100}
+              height={100}
+              className="clip-path-group-2"
+              alt="Clip path group"
+              src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/clip-path-group-8@2x.png"
+            />
+            <div className="text-wrapper-5">PARTNER N</div>
+            <Image
+              width={100}
+              height={100}
+              className="clip-path-group-3"
+              alt="Clip path group"
+              src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/clip-path-group-8@2x.png"
+            />
+            <div className="text-wrapper-6">PARTNER N+1</div>
+            <Image
+              width={100}
+              height={100}
+              className="clip-path-group-4"
+              alt="Clip path group"
+              src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/clip-path-group-8@2x.png"
+            />
+          </div>
+          <div className="overlap">
+            <div className="text-wrapper-7">WHY?</div>
+            <div className="text-wrapper-8">Because,</div>
+            <div className="group">
+              <p className="p">
+                Mobile application usage accounts for up to 88% of digital media
+                time.
+              </p>
+              <p className="text-wrapper-9">
+                Web apps account for 29%, while Desktop apps, ssh, others,
+                account for 4%
+              </p>
+            </div>
+            <div className="overlap-2">
+              <p className="text-wrapper-10">
+                A big proportion of the smartphone audience goes to mobile app
+                users
+              </p>
+              <div className="text-wrapper-11">70%</div>
+            </div>
+            <div className="frame">
+              <p className="text-wrapper-12">
+                Hence, web3 and crypto can reach the next 1billion users in
+                their comfort zone
+              </p>
+            </div>
+            <Image
+              width={100}
+              height={100}
+              className="pie-chart"
+              alt="Pie chart"
+              src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/pie-chart@2x.png"
+            />
+          </div>
+          <div className="overlap-3">
+            <div className="group-2">
+              <div className="ellipse" />
+              <div className="ellipse-2" />
+              <div className="rectangle" />
+            </div>
+            <div className="v-aa-riance">V AA RIANCE</div>
+            <div className="element-sdk">4337 SDK</div>
+            <div className="text-wrapper-13">variance labs</div>
+            <div className="group-3">
+              <div className="ellipse-3" />
+              <div className="ellipse-4" />
+              <div className="ellipse-5" />
+              <div className="ellipse-6" />
+              <div className="ellipse-7" />
+            </div>
+            <div className="frame-2">
+              <div className="text-wrapper-14">account</div>
+              <Image
+                width={100}
+                height={100}
+                className="line"
+                alt="Line"
+                src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/line-1.svg"
+              />
+            </div>
+            <div className="frame-3">
+              <div className="text-wrapper-14">ABSTRACTION</div>
+              <Image
+                width={100}
+                height={100}
+                className="line"
+                alt="Line"
+                src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/line-1.svg"
+              />
+            </div>
+          </div>
+          <p className="a-step-forward">
+            A step forward
+            <br />
+            migrating web3 <br />
+            to mobile
+          </p>
+          <p className="text-wrapper-15">
+            Open source account abstraction SDK written in dart that empowers
+            developers to create mobile-native Web3 applications with ease.
+          </p>
+          <p className="text-wrapper-16">
+            Enhancing the overall user experience and opening up new
+            possibilities for the Web3 ecosystem on mobile platforms.
+          </p>
+          <div className="frame-4">
+            <div className="frame-5">
+              <div className="frame-6">
+                <Image
+                  width={100}
+                  height={100}
+                  className="clip-path-group-5"
+                  alt="Clip path group"
+                  src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/clip-path-group-13@2x.png"
+                />
+                <div className="text-wrapper-17">Smart choice</div>
+              </div>
+              <p className="text-wrapper-18">
+                We focus on delivering a seamless mobile experience. Our
+                project’s core value is to make Dapps accessible and
+                user-friendly on smartphones, aligning with the increasing trend
+                of mobile usage.
+              </p>
+            </div>
+            <div className="frame-5">
+              <div className="frame-6">
+                <Image
+                  width={100}
+                  height={100}
+                  className="clip-path-group-6"
+                  alt="Clip path group"
+                  src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/clip-path-group-14@2x.png"
+                />
+                <div className="text-wrapper-17">Simplicity</div>
+              </div>
+              <p className="text-wrapper-18">
+                Tools that empower developers to create mobile-native
+                self-custodial wallets and applications with ease.
+              </p>
+            </div>
+            <div className="frame-5">
+              <div className="frame-6">
+                <Image
+                  width={100}
+                  height={100}
+                  className="group-4"
+                  alt="Group"
+                  src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/group@2x.png"
+                />
+                <div className="text-wrapper-17">Security</div>
+              </div>
+              <p className="text-wrapper-19">
+                We focus on delivering a seamless mobile experience. Our
+                project’s core value is to make Dapps accessible and
+                user-friendly on smartphones, aligning with the increasing trend
+                of mobile usage.
+              </p>
+            </div>
+          </div>
+          <p className="safe-core-protocol">
+            safe {"{"}core{"}"} protocol, the modular account abstraction stack
+          </p>
+          <p className="text-wrapper-20">
+            Leveraging the modular architecture of the Safe protocol with all
+            the benefits
+          </p>
+          <div className="overlap-4">
+            <div className="ellipse-8" />
+            <div className="safe-protocol">
+              SAFE <br />
+              PROTOCOL
+            </div>
+          </div>
+          <Image
+            width={100}
+            height={100}
+            className="vector"
+            alt="Vector"
+            src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/vector-1.svg"
+          />
+          <div className="text-wrapper-21">
+            Multi-signature schemes (secp256r1, secp256k1)
+          </div>
+          <p className="text-wrapper-22">
+            Allowing the adoption of multiple signature schemes including
+            passkeys for transactions
+          </p>
+          <div className="overlap-5">
+            <div className="ellipse-8" />
+            <div className="signature-schemes">
+              SIGNATURE <br />
+              SCHEMES
+            </div>
+          </div>
+          <Image
+            width={100}
+            height={100}
+            className="vector-2"
+            alt="Vector"
+            src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/vector-2.svg"
+          />
+          <div className="text-wrapper-23">EIP 4337 smart-accounts</div>
+          <p className="text-wrapper-24">
+            Unlocking limitless User Experience with smart-contract wallets for
+            the next 1 billion.
+          </p>
+          <div className="overlap-6">
+            <div className="ellipse-8" />
+            <div className="smart-wallet">
+              SMART <br />
+              WALLET
+            </div>
+          </div>
+          <Image
+            width={100}
+            height={100}
+            className="vector-3"
+            alt="Vector"
+            src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/vector-3.svg"
+          />
+          <div className="text-wrapper-25">ROADMAP</div>
+          <Image
+            width={100}
+            height={100}
+            className="clip-path-group-7"
+            alt="Clip path group"
+            src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/clip-path-group@2x.png"
+          />
+          <Image
+            width={100}
+            height={100}
+            className="clip-path-group-8"
+            alt="Clip path group"
+            src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/clip-path-group-1@2x.png"
+          />
+          <Image
+            width={100}
+            height={100}
+            className="vector-4"
+            alt="Vector"
+            src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/vector-2-1.svg"
+          />
+          <div className="overlap-7">
+            <Image
+              width={100}
+              height={100}
+              className="vector-5"
+              alt="Vector"
+              src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/vector-3-1.svg"
+            />
+            <Image
+              width={100}
+              height={100}
+              className="vector-6"
+              alt="Vector"
+              src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/vector-4.svg"
+            />
+          </div>
+          <Image
+            width={100}
+            height={100}
+            className="clip-path-group-9"
+            alt="Clip path group"
+            src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/clip-path-group-2@2x.png"
+          />
+          <Image
+            width={100}
+            height={100}
+            className="clip-path-group-10"
+            alt="Clip path group"
+            src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/clip-path-group-3@2x.png"
+          />
+          <Image
+            width={100}
+            height={100}
+            className="vector-7"
+            alt="Vector"
+            src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/vector-3-2.svg"
+          />
+          <Image
+            width={100}
+            height={100}
+            className="vector-8"
+            alt="Vector"
+            src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/vector-3-2.svg"
+          />
+          <div className="text-wrapper-26">Wallet Signers</div>
+          <p className="allowing-users-to">
+            Allowing users to seamlessly switch to hardware-based signatures
+            when needed.
+            <br />
+            Streamline key management for users relying on EOA (Externally Owned
+            Account) signers
+            <br />
+            Interoperate with both passkeys signers and EOA (Externally Owned
+            Account) signers
+          </p>
+          <Image
+            width={100}
+            height={100}
+            className="clip-path-group-11"
+            alt="Clip path group"
+            src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/clip-path-group-4@2x.png"
+          />
+          <div className="text-wrapper-27">4337 Providers</div>
+          <p className="abstract">
+            Abstract complexities associated with bundlers, Entrypoint, and
+            paymasters.
+            <br />
+            Make integration of smart accounts straightforward with just a few
+            lines of code.
+            <br />
+            Empower developers to build user operations in Dart seamlessly.
+          </p>
+          <Image
+            width={100}
+            height={100}
+            className="clip-path-group-12"
+            alt="Clip path group"
+            src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/clip-path-group-5@2x.png"
+          />
+          <div className="text-wrapper-28">Safe Core Utils</div>
+          <p className="safe-helpers-that">
+            Safe helpers that enable developers to work with Safe modular
+            accounts.
+            <br />
+            Compatibility with wallet provider.
+            <br />
+            Gelato as an alternative to 4337 via alt mempool.
+          </p>
+          <Image
+            width={100}
+            height={100}
+            className="clip-path-group-13"
+            alt="Clip path group"
+            src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/clip-path-group-6@2x.png"
+          />
+          <div className="text-wrapper-29">Add-ons</div>
+          <p className="enable-smart-wallet">
+            Enable smart wallet developers to provide users with a streamlined
+            means of connecting their wallets to Dapps on the web.
+            <br />
+            Native Cross-Chain support with CCIP interface.
+          </p>
+          <Image
+            width={100}
+            height={100}
+            className="clip-path-group-14"
+            alt="Clip path group"
+            src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/clip-path-group-7@2x.png"
+          />
+          <div className="group-5">
+            <div className="ellipse" />
+            <div className="ellipse-2" />
+            <div className="rectangle" />
+          </div>
+          <div className="text-wrapper-30">WEB3</div>
+          <div className="text-wrapper-31">mobile</div>
+          <div className="frame-7">
+            <div className="frame-8">
+              <Image
+                width={100}
+                height={100}
+                className="img-2"
+                alt="Github"
+                src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/github.svg"
+              />
+              <div className="text-wrapper-32">Github</div>
+            </div>
+            <div className="frame-8">
+              <Image
+                width={100}
+                height={100}
+                className="img-2"
+                alt="Note edit"
+                src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/note-edit.svg"
+              />
+              <div className="text-wrapper-32">Documentation</div>
+            </div>
+          </div>
+          <div className="frame-9">
+            <div className="frame-8">
+              <Image
+                width={100}
+                height={100}
+                className="img-2"
+                alt="Github"
+                src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/github-1.svg"
+              />
+              <div className="text-wrapper-32">Github</div>
+            </div>
+            <div className="frame-8">
+              <Image
+                width={100}
+                height={100}
+                className="img-2"
+                alt="Note edit"
+                src="https://cdn.animaapp.com/projects/655f0e9ac9857335bc9acfb3/releases/655f0f518d913955299b6249/img/note-edit.svg"
+              />
+              <div className="text-wrapper-32">Documentation</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
