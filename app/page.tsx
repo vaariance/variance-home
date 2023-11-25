a962c5d623c29be644efdebed67624b8a2c8f394
miracleonyenma <miracleficient@gmail.com>
2023-11-25 10:29:09 +0100

    updates: add features section to home page

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,30 @@ const Home = () => {
       img: "/img/security.svg",
       title: "Security",
       description:
-        "We focus on delivering a seamless mobile experience. Our project’s core value is to make Dapps accessible and user-friendly on smartphones, aligning with the increasing trend of mobile usage.",
+        "We focus on delivering a seamless mobile experience. Oleur project’s core value is to make Dapps accessible and user-friendly on smartphones, aligning with the increasing trend of mobile usage.",
+    },
+  ];
+  const featureList = [
+    {
+      title: "Safe Protocol",
+      description: [
+        "safe {core} protocol, the modular account abstraction stack",
+        "Leveraging the modular architecture of the Safe protocol with all the benefits",
+      ],
+    },
+    {
+      title: "signature schemes",
+      description: [
+        "Multi-signature schemes (secp256r1, secp256k1)",
+        "Allowing the adoption of multiple signature schemes including passkeys for transactions",
+      ],
+    },
+    {
+      title: "smart wallet",
+      description: [
+        "EIP 4337 smart-accounts",
+        "Unlocking limitless User Experience with smart-contract wallets for the next 1 billion.",
+      ],
     },
   ];
   return (
@@ -69,6 +92,53 @@ const Home = () => {
                   <p className="benefit-list__item-description">
                     {benefit.description}
                   </p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+      <section className="site-section features-section">
+        <div className="wrapper">
+          <ul className="feature-list">
+            {featureList.map((feature) => (
+              <li className="feature-list__item" key={feature.title}>
+                <div className="feature-list__item-header">
+                  <h3 className="feature-list__item-title">{feature.title}</h3>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="163"
+                    height="163"
+                    viewBox="0 0 163 163"
+                    fill="none"
+                  >
+                    <circle
+                      cx="81.1025"
+                      cy="81.1025"
+                      r="80.5"
+                      transform="rotate(-0.0734336 81.1025 81.1025)"
+                      fill="#5E5E5E"
+                      stroke="white"
+                      stroke-dasharray="10 10"
+                    />
+                  </svg>
+                </div>
+                <div className="feature-list__content">
+                  <p>{feature.description[0]}</p>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="1"
+                    height="192"
+                    viewBox="0 0 1 192"
+                    fill="none"
+                  >
+                    <path
+                      d="M0.5 0.5V191.5"
+                      stroke="white"
+                      stroke-dasharray="10 10"
+                    />
+                  </svg>
+                  <p>{feature.description[1]}</p>
                 </div>
               </li>
             ))}
