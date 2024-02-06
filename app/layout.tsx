a52fb2abc1afa250453b336460f3a6b85fff741a
Peter Anyaogu <anyaogupeter601@gmail.com>
2024-02-06 22:23:20 +0100

    add scroll smooth

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <PalleteProvider>
         <App>{children}</App>
       </PalleteProvider>
